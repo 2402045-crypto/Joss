04bb81c5d4ace57855f0a4fc5b9d41917e998b9f
HanaG07 <2402051@utrivieramaya.edu.mx>
2026-07-08 08:44:47 -0500

    Cambio de ruta

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import SearchMechanics from '../components/SearchMechanics.vue'
 import MapSearch from '../components/MapSearch.vue'
 import HelpView from '../components/HelpView.vue'
 import TallerRegister from '../components/TallerRegister.vue'
+import SearchTaller from '../components/SearchTaller.vue'
 
 const routes = [
   { path: '/', name: 'landing', component: LandingPage },
@@ -15,9 +16,10 @@ const routes = [
   { path: '/register', name: 'register', component: Register },
   { path: '/home', name: 'home', component: HomeView },
   { path: '/search', name: 'search', component: SearchMechanics },
-  { path: '/mapsearch', name: 'mapsearch', component: MapSearch },
+  { path: '/buscarTaller', name: 'buscarTaller', component: SearchTaller },
   { path: '/help', name: 'help', component: HelpView},
   { path: '/taller-register', name: 'taller-register', component: TallerRegister },
+  { path: '/maps', name: 'maps', component: MapSearch },
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
