6d313bb53343cd8cb963c0185266a2be59be989e
JariffA69 <2402064@utrivieramaya.edu.mx>
2026-07-13 22:51:29 -0500

    Se agregó Perfil vue para el módulo donde el usuario podrá editar o actualizar sus datos de registro y se agregó su ruta en index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import SearchTaller from '../components/SearchTaller.vue'
 import PerfilTaller from '../components/PerfilTaller.vue'
 import Citas from '../components/Citas.vue'
 import MisCitas from '../components/MisCitas.vue'
+import Perfil from '../components/Perfil.vue'
 
 const routes = [
   { path: '/', name: 'landing', component: LandingPage },
@@ -25,6 +26,7 @@ const routes = [
   { path: '/maps', name: 'maps', component: MapSearch },
   { path: '/perfiltaller', name: 'perfil', component: PerfilTaller },
   { path: '/miscitas', name: 'miscitas', component: MisCitas },
+  { path: '/perfil', name: 'perfil', component: Perfil },
   
   // RUTAS PROTEGIDAS (Solo para Mecánicos - Rol 2)
   { 
